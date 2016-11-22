3a8da71d3aedf3031e6adfafda4690398c3b3177
Brad Efting <bradefting@gmail.com>
2016-11-22 16:49:11 -0700

    refactor express code

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,9 @@
 "use strict";
 
+var fs = require('fs');
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+
 var express = require('express');
 var app = express();
 
@@ -10,24 +14,39 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 //read pet.json this way to only "get"/fs.read/write when making changes(pt4)
-var pets = require('./pets.json');
-
-// app.use(express.static('public'));
+// var pets = require('./pets.json');
 
 app.get('/pets', function(req, res){
-  res.send(pets);
+  fs.readFile('petsPath', 'utf8', function(err, data){
+    if (err){
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+    var pets = JSON.parse(data);
+
+    res.send(pets);
+  });
 });
 
 app.get('/pets/:id', function(req, res){
-  var petIndex = req.params.id;
-  var numPets = pets.length;
+  fs.readFile('petsPath', 'utf8', function(err, data){
+    if (err){
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+    var pets = JSON.parse(data);
 
-  if(petIndex >=0 && petIndex<numPets){
-    res.send(pets[petIndex]);
-  }
-  else{
-    return res.sendStatus(404);
-  }
+    //get url parameter called id
+    var petIndex = req.params.id;
+    var numPets = pets.length;
+
+    if(petIndex >=0 && petIndex<numPets){
+      res.send(pets[petIndex]);
+    }
+    else{
+      return res.sendStatus(404);
+    }
+  });
 });
 
 app.listen('8080', function(){
